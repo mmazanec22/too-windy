98c67e0de40d7fd528401635e4d0ff6b0d213e20
mmazanec22 <melanie.mazanec@gmail.com>
2016-12-21 15:36:13 -0600

    Refactor and add notes

--- get_local_wind_speed.py
+++ get_local_wind_speed.py
@@ -2,19 +2,9 @@ import requests
 import sys
 import time
 
-# MAKE SURE LIMIT IS NOT EXCEEDED
 
-# track total num calls and date of last call made; if current call is different date, then clear the total num of API calls
 
-total_num_calls = 0
-# date_last_call = TODAY SOMEHOW
-
-def exceed_limit():
-  if total_num_calls >= 1000:
-    return true
-  else:
-    return false
-
+# modify this to take a zip code arg rather than lat lon - need to use some converter
 
 
 # MAKE API CALL
@@ -24,41 +14,64 @@ def return_data_from_api(url):
   if response.status_code == 200:
     return response.json()
 
-# def get_current_wind_speed_from_return_data(json_data):
-#   return json_data["wind"]["speed"]
-
-def get_current_wind_speed_by_lat_lon(lat, lon):
+def get_all_data(lat, lon):
   try:
     api_key = ""
     with open('dark_sky_api_key.txt', 'r') as myfile:
       api_key = myfile.read().replace('\n', '')
     url = "https://api.darksky.net/forecast/" + api_key + "/" + str(lat) +"," + str(lon)
     data = return_data_from_api(url)
-    return data['hourly']['data']
   except:
     return "Do you even latitude and longitude, bro?"
 
 
 
+# PARSE DATA
+
+def current_wind_speed(all_data):
+  return all_data['hourly']['data'][0]['windSpeed']
+
+# def warnings:
+  # there's a warning somewhere in "all data"
+
+def hourly_data(all_data):
+  return data['hourly']['data']
+
+def get_highest_speed_before_midnight_with_time(all_data):
+  highest_speed = 0.0
+  time_of_highest_speed = ""
+  # modify this so it only looks at times before midnight - until time ends in 00:00:00
+  for k in all_data['hourly']['data']:
+    if k['windSpeed'] > highest_speed:
+      highest_speed = k['windSpeed']
+      time_of_highest_speed = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(k['time']))
+  return "The strongest wind today will be " + str(highest_speed) + " at " + time_of_highest_speed
+
+
+
+# MAKE SURE LIMIT IS NOT EXCEEDED
+
+# track total_num_calls and date_last_call in a text file
+# if current call is different date, then clear the total num of API calls
+
+def exceed_limit():
+  if total_num_calls >= 1000:
+    return true
+  else:
+    return false
+
+
+
 # RUN THE WHOLE THING
 
-hourly_data = get_current_wind_speed_by_lat_lon(sys.argv[1], sys.argv[2])
+all_data = get_all_data_by_lat_lon(sys.argv[1], sys.argv[2])
 # print to text file:
   # current speed
   # highest speed before midnight tonight w/time
   # weather warnings
   # all speeds in next 24 hours w/times
 
-for k in hourly_data:
-  print(k['time'])
-  print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(k['time'])))
-  print(k)
-  print("**********")
 
-
-# until time ends in 00:00:00, loop through and update highest wind speed
-
-print(hourly_data[0]['windSpeed'])
 
 # return current speed
 # if we have not exceeded the 1000/day limit
